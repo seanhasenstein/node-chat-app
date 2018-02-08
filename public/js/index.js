673f4234e23677005be1e9b1c78f9fc758beb285
Sean Hasenstein <seanhasenstein@gmail.com>
2018-02-08 16:07:11 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'Nate',
-		text: 'This is a message from the client'
-	});
 });
 
 socket.on('disconnect', function () {
